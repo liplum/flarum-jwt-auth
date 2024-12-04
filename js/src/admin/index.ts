c2e0de9fd3a0390966fedf88e5a8c2f2b2ba5218
Liplum <Li_plum@outlook.com>
2024-12-04 17:08:31 +0800

    webpack rebuilding

--- js/src/admin/index.ts
+++ js/src/admin/index.ts
@@ -1,92 +1,92 @@
 import app from 'flarum/admin/app';
 
 app.initializers.add('jwt-auth', () => {
-    app.extensionData
-        .for('liplum-jwt-auth')
-        .registerSetting({
-            setting: 'jwt-auth.cookieName',
-            type: 'text',
-            label: app.translator.trans('liplum-jwt-auth.admin.settings.cookieName'),
-        })
-        .registerSetting({
-            setting: 'jwt-auth.actorId',
-            type: 'text',
-            label: app.translator.trans('liplum-jwt-auth.admin.settings.actorId'),
-            help: app.translator.trans('liplum-jwt-auth.admin.settings.actorIdHelp'),
-            placeholder: '1',
-        })
-        .registerSetting({
-            setting: 'jwt-auth.audience',
-            type: 'text',
-            label: app.translator.trans('liplum-jwt-auth.admin.settings.audience'),
-            help: app.translator.trans('liplum-jwt-auth.admin.settings.audienceHelp'),
-        })
-        .registerSetting({
-            setting: 'jwt-auth.publicKey',
-            type: 'textarea',
-            label: app.translator.trans('liplum-jwt-auth.admin.settings.publicKey'),
-            help: app.translator.trans('liplum-jwt-auth.admin.settings.publicKeyHelp'),
-        })
-        .registerSetting({
-            setting: 'jwt-auth.publicKeyAlgorithm',
-            type: 'text',
-            label: app.translator.trans('liplum-jwt-auth.admin.settings.publicKeyAlgorithm'),
-            help: app.translator.trans('liplum-jwt-auth.admin.settings.publicKeyAlgorithmHelp'),
-        })
-        .registerSetting({
-            setting: 'jwt-auth.expirationLeeway',
-            type: 'number',
-            label: app.translator.trans('liplum-jwt-auth.admin.settings.expirationLeeway'),
-            help: app.translator.trans('liplum-jwt-auth.admin.settings.expirationLeewayHelp'),
-            placeholder: '0',
-        })
-        .registerSetting({
-            setting: 'jwt-auth.usernameTemplate',
-            type: 'text',
-            label: app.translator.trans('liplum-jwt-auth.admin.settings.usernameTemplate'),
-            help: app.translator.trans('liplum-jwt-auth.admin.settings.usernameTemplateHelp', {
-                sub: '{sub}',
-            }),
-        })
-        .registerSetting({
-            setting: 'jwt-auth.emailTemplate',
-            type: 'text',
-            label: app.translator.trans('liplum-jwt-auth.admin.settings.emailTemplate'),
-            help: app.translator.trans('liplum-jwt-auth.admin.settings.emailTemplateHelp', {
-                sub: '{sub}',
-            }),
-        })
-        .registerSetting({
-            setting: 'jwt-auth.registrationHook',
-            type: 'text',
-            label: app.translator.trans('liplum-jwt-auth.admin.settings.registrationHook'),
-            help: app.translator.trans('liplum-jwt-auth.admin.settings.registrationHookHelp', {
-                sub: '{sub}',
-            }),
-        })
-        .registerSetting({
-            setting: 'jwt-auth.authorizationHeader',
-            type: 'text',
-            label: app.translator.trans('liplum-jwt-auth.admin.settings.authorizationHeader'),
-            help: app.translator.trans('liplum-jwt-auth.admin.settings.authorizationHeaderHelp'),
-        })
-        .registerSetting({
-            setting: 'jwt-auth.hiddenIframe',
-            type: 'text',
-            label: app.translator.trans('liplum-jwt-auth.admin.settings.hiddenIframe'),
-            help: app.translator.trans('liplum-jwt-auth.admin.settings.hiddenIframeHelp'),
-        })
-        .registerSetting({
-            setting: 'jwt-auth.autoLoginDelay',
-            type: 'number',
-            label: app.translator.trans('liplum-jwt-auth.admin.settings.autoLoginDelay'),
-            help: app.translator.trans('liplum-jwt-auth.admin.settings.autoLoginDelayHelp'),
-            placeholder: '2000',
-        })
-        .registerSetting({
-            setting: 'jwt-auth.logoutRedirect',
-            type: 'text',
-            label: app.translator.trans('liplum-jwt-auth.admin.settings.logoutRedirect'),
-            help: app.translator.trans('liplum-jwt-auth.admin.settings.logoutRedirectHelp'),
-        });
+  app.extensionData
+    .for('liplum-jwt-auth')
+    .registerSetting({
+      setting: 'jwt-auth.cookieName',
+      type: 'text',
+      label: app.translator.trans('liplum-jwt-auth.admin.settings.cookieName'),
+    })
+    .registerSetting({
+      setting: 'jwt-auth.actorId',
+      type: 'text',
+      label: app.translator.trans('liplum-jwt-auth.admin.settings.actorId'),
+      help: app.translator.trans('liplum-jwt-auth.admin.settings.actorIdHelp'),
+      placeholder: '1',
+    })
+    .registerSetting({
+      setting: 'jwt-auth.audience',
+      type: 'text',
+      label: app.translator.trans('liplum-jwt-auth.admin.settings.audience'),
+      help: app.translator.trans('liplum-jwt-auth.admin.settings.audienceHelp'),
+    })
+    .registerSetting({
+      setting: 'jwt-auth.publicKey',
+      type: 'textarea',
+      label: app.translator.trans('liplum-jwt-auth.admin.settings.publicKey'),
+      help: app.translator.trans('liplum-jwt-auth.admin.settings.publicKeyHelp'),
+    })
+    .registerSetting({
+      setting: 'jwt-auth.publicKeyAlgorithm',
+      type: 'text',
+      label: app.translator.trans('liplum-jwt-auth.admin.settings.publicKeyAlgorithm'),
+      help: app.translator.trans('liplum-jwt-auth.admin.settings.publicKeyAlgorithmHelp'),
+    })
+    .registerSetting({
+      setting: 'jwt-auth.expirationLeeway',
+      type: 'number',
+      label: app.translator.trans('liplum-jwt-auth.admin.settings.expirationLeeway'),
+      help: app.translator.trans('liplum-jwt-auth.admin.settings.expirationLeewayHelp'),
+      placeholder: '0',
+    })
+    .registerSetting({
+      setting: 'jwt-auth.usernameTemplate',
+      type: 'text',
+      label: app.translator.trans('liplum-jwt-auth.admin.settings.usernameTemplate'),
+      help: app.translator.trans('liplum-jwt-auth.admin.settings.usernameTemplateHelp', {
+        sub: '{sub}',
+      }),
+    })
+    .registerSetting({
+      setting: 'jwt-auth.emailTemplate',
+      type: 'text',
+      label: app.translator.trans('liplum-jwt-auth.admin.settings.emailTemplate'),
+      help: app.translator.trans('liplum-jwt-auth.admin.settings.emailTemplateHelp', {
+        sub: '{sub}',
+      }),
+    })
+    .registerSetting({
+      setting: 'jwt-auth.registrationHook',
+      type: 'text',
+      label: app.translator.trans('liplum-jwt-auth.admin.settings.registrationHook'),
+      help: app.translator.trans('liplum-jwt-auth.admin.settings.registrationHookHelp', {
+        sub: '{sub}',
+      }),
+    })
+    .registerSetting({
+      setting: 'jwt-auth.authorizationHeader',
+      type: 'text',
+      label: app.translator.trans('liplum-jwt-auth.admin.settings.authorizationHeader'),
+      help: app.translator.trans('liplum-jwt-auth.admin.settings.authorizationHeaderHelp'),
+    })
+    .registerSetting({
+      setting: 'jwt-auth.hiddenIframe',
+      type: 'text',
+      label: app.translator.trans('liplum-jwt-auth.admin.settings.hiddenIframe'),
+      help: app.translator.trans('liplum-jwt-auth.admin.settings.hiddenIframeHelp'),
+    })
+    .registerSetting({
+      setting: 'jwt-auth.autoLoginDelay',
+      type: 'number',
+      label: app.translator.trans('liplum-jwt-auth.admin.settings.autoLoginDelay'),
+      help: app.translator.trans('liplum-jwt-auth.admin.settings.autoLoginDelayHelp'),
+      placeholder: '2000',
+    })
+    .registerSetting({
+      setting: 'jwt-auth.logoutRedirect',
+      type: 'text',
+      label: app.translator.trans('liplum-jwt-auth.admin.settings.logoutRedirect'),
+      help: app.translator.trans('liplum-jwt-auth.admin.settings.logoutRedirectHelp'),
+    });
 });
--- js/src/forum/index.ts
+++ js/src/forum/index.ts
@@ -1,103 +1,103 @@
 import app from 'flarum/forum/app';
-import {extend} from 'flarum/common/extend';
+import { extend } from 'flarum/common/extend';
 import Button from 'flarum/common/components/Button';
 import LinkButton from 'flarum/common/components/LinkButton';
 import SessionDropdown from 'flarum/forum/components/SessionDropdown';
 import ForumApplication from 'flarum/forum/ForumApplication';
 
 app.initializers.add('jwt-auth', () => {
-    extend(SessionDropdown.prototype, 'items', function (items) {
-        const href = app.forum.attribute<string | false>('logoutRedirect');
+  extend(SessionDropdown.prototype, 'items', function (items) {
+    const href = app.forum.attribute<string | false>('logoutRedirect');
 
-        // False is used to explicitly say the logout button should be hidden without any replacement
-        if (href || href === false) {
-            items.remove('logOut');
-        }
+    // False is used to explicitly say the logout button should be hidden without any replacement
+    if (href || href === false) {
+      items.remove('logOut');
+    }
 
-        if (href) {
-            items.add('logOutLink', LinkButton.component({
-                icon: 'fas fa-sign-out-alt',
-                href,
-            }, app.translator.trans('core.forum.header.log_out_button')));
-        }
-    });
+    if (href) {
+      items.add('logOutLink', LinkButton.component({
+        icon: 'fas fa-sign-out-alt',
+        href,
+      }, app.translator.trans('core.forum.header.log_out_button')));
+    }
+  });
 
-    extend(ForumApplication.prototype, 'mount', function () {
-        const url = app.forum.attribute<string>('jwtIframe');
+  extend(ForumApplication.prototype, 'mount', function () {
+    const url = app.forum.attribute<string>('jwtIframe');
 
-        if (!url) {
+    if (!url) {
+      return;
+    }
+
+    let bootTime = new Date();
+    let hasPromptedLogin = false;
+    let hasPromptedLogout = false;
+
+    const parsedUrl = new URL(url);
+
+    window.addEventListener('message', (event) => {
+      if (event.origin !== parsedUrl.origin) {
+        return;
+      }
+
+      if (typeof event.data !== 'object' || !event.data.hasOwnProperty('jwtSessionState')) {
+        return;
+      }
+
+      const state = event.data.jwtSessionState;
+
+      function showRefreshAlert(type: string, translation: string) {
+        app.alerts.show({
+          type,
+          controls: [
+            Button.component({
+              className: 'Button Button--link',
+              onclick() {
+                window.location.reload();
+              },
+            }, app.translator.trans('liplum-jwt-auth.forum.alert.refresh')),
+          ],
+        }, app.translator.trans('liplum-jwt-auth.forum.alert.' + translation));
+      }
+
+      switch (state) {
+        case 'login':
+          if (app.session.user || hasPromptedLogin) {
             return;
-        }
+          }
 
-        let bootTime = new Date();
-        let hasPromptedLogin = false;
-        let hasPromptedLogout = false;
+          // After how many milliseconds should the page refresh automatically if the user was auto-connected
+          if ((new Date()).getTime() - bootTime.getTime() < app.forum.attribute<number>('autoLoginDelay')) {
+            window.location.reload();
+            return;
+          }
 
-        const parsedUrl = new URL(url);
+          hasPromptedLogin = true;
 
-        window.addEventListener('message', (event) => {
-            if (event.origin !== parsedUrl.origin) {
-                return;
-            }
+          showRefreshAlert('success', 'login');
 
-            if (typeof event.data !== 'object' || !event.data.hasOwnProperty('jwtSessionState')) {
-                return;
-            }
+          break;
+        case 'logout':
+          if (!app.session.user || hasPromptedLogout) {
+            return;
+          }
 
-            const state = event.data.jwtSessionState;
+          hasPromptedLogout = true;
 
-            function showRefreshAlert(type: string, translation: string) {
-                app.alerts.show({
-                    type,
-                    controls: [
-                        Button.component({
-                            className: 'Button Button--link',
-                            onclick() {
-                                window.location.reload();
-                            },
-                        }, app.translator.trans('liplum-jwt-auth.forum.alert.refresh')),
-                    ],
-                }, app.translator.trans('liplum-jwt-auth.forum.alert.' + translation));
-            }
+          showRefreshAlert('error', 'logout');
 
-            switch (state) {
-                case 'login':
-                    if (app.session.user || hasPromptedLogin) {
-                        return;
-                    }
+          break;
+      }
+    }, false);
 
-                    // After how many milliseconds should the page refresh automatically if the user was auto-connected
-                    if ((new Date()).getTime() - bootTime.getTime() < app.forum.attribute<number>('autoLoginDelay')) {
-                        window.location.reload();
-                        return;
-                    }
+    const iframe = document.createElement('iframe');
+    iframe.src = url;
+    iframe.width = '0';
+    iframe.height = '0';
+    iframe.style.position = 'absolute';
+    iframe.style.top = '-1000px;'
+    iframe.style.left = '-1000px';
 
-                    hasPromptedLogin = true;
-
-                    showRefreshAlert('success', 'login');
-
-                    break;
-                case 'logout':
-                    if (!app.session.user || hasPromptedLogout) {
-                        return;
-                    }
-
-                    hasPromptedLogout = true;
-
-                    showRefreshAlert('error', 'logout');
-
-                    break;
-            }
-        }, false);
-
-        const iframe = document.createElement('iframe');
-        iframe.src = url;
-        iframe.width = '0';
-        iframe.height = '0';
-        iframe.style.position = 'absolute';
-        iframe.style.top = '-1000px;'
-        iframe.style.left = '-1000px';
-
-        document.body.appendChild(iframe);
-    });
+    document.body.appendChild(iframe);
+  });
 });
