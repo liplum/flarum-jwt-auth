c6280bebba7e579b0f5d001ad1c68fe5c1612a1e
Liplum <Li_plum@outlook.com>
2024-12-07 04:15:39 +0800

    updated migration

--- migrations/2024_12_07_000000_add_jwt_subject_in_users_table.php
+++ migrations/2024_12_07_000000_add_jwt_subject_in_users_table.php
@@ -1,7 +1,20 @@
 <?php
 
-use Flarum\Database\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Schema\Builder;
 
-return Migration::addColumns('users', [
-  'last_avatar_url' => ['string', 'nullable' => true],
-]);
+return [
+  'up' => function (Builder $schema) {
+    $schema->table('users', function (Blueprint $table) use ($schema) {
+      if (!$schema->hasColumn('users', 'jwt_subject')) {
+        $table->text('jwt_subject')->nullable();
+      }
+    });
+  },
+
+  'down' => function (Builder $schema) {
+    $schema->table('users', function (Blueprint $table) {
+      $table->dropColumn('jwt_subject');
+    });
+  },
+];
